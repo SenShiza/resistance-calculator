bb93f650aa94d2394153762cfc9c80953d16199d
Shiza <88404459+SenShiza@users.noreply.github.com>
2023-10-27 18:23:08 -0400

    Can now calculate parallel resistances
    Finished the parallel method where it calculates all the resistance given in an array by the user. 
    
    Added a restart method where asks the user if they want to calculate more resistances
    
    Added a exit method where gives thanks to the user for using the program and exits

--- resistanceCalculator.java
+++ resistanceCalculator.java
@@ -71,8 +71,47 @@ public class resistanceCalculator {
 
     public static void parallel() {
 
-        // WIP
-        JOptionPane.showMessageDialog(null, "We're sorry. This feature is not currently available.");
+        String input;
+        int amount = 0;
+        float total = 0;
+
+        // Asks the user how many resistances wants to calculate in order to create an array of that size
+        input = JOptionPane.showInputDialog("How many resistances would you like to calculate?");
+        amount = Integer.parseInt(input);
+
+        // Creates an array of a given size from the user
+        float[] Resistance = new float[amount];
+
+        // Adds each resistance value into the Resistance[] array
+        for (int i = 0; i < amount; i++) {
+            
+            int temp = i + 1;
+            Resistance[i] = Float.parseFloat(JOptionPane.showInputDialog("Please enter the value of resistance number " + temp + " (in kΩ)"));
+
+        }
+
+        // Turns each resistance into its reciprocal
+        for (int j = 0; j < amount; j++) {
+
+            Resistance[j] = 1/Resistance[j];
+        }
+
+        // Sums all resistances
+        for (int k = 0; k < amount; k++) {
+
+            total = total + Resistance[k];
+
+        }
+
+        // Turns the total value into the reciprocal
+        total = 1 / total;
+
+        // Shows the result in a GUI
+        JOptionPane.showMessageDialog(null, "Your result is: " + total + "kΩ");
+
+        // Redirects the user to the Restart() method
+        restart();
+
     }
 
     public static void series() {
@@ -87,5 +126,30 @@ public class resistanceCalculator {
         // Redirects the user back to the confirmation() method
 
         confirmation();
+    }
+    
+    public static void restart() {
+
+        //Asks the user whether they want to calculate more resistances or not
+        int restart;
+
+        restart = JOptionPane.showConfirmDialog(null, "Would you like to calculate more resistances?",
+                                "One more time?", JOptionPane.YES_NO_OPTION);
+        
+        if (restart == JOptionPane.YES_OPTION) {
+
+            confirmation();
+        }
+        else {
+
+            exit();
+        }
+    }
+    
+    public static void exit() {
+
+        // Gives a thank you message before closing the program
+        JOptionPane.showMessageDialog(null, "Thank you for using the Resistance Calculator Program by SenShiza");
+        JOptionPane.showMessageDialog(null, "Press OK to exit");
     }
 }
